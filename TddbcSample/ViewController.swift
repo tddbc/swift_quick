de244cdc39546386da20d19b9c94b6e9a22cae09
135yshr <isago@praisefirst.co.jp>
2014-10-09 20:03:07 +0900

    Initial commit.

---
+++ TddbcSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TddbcSample
+//
+//  Created by 135yshr on 2014/10/09.
+//  Copyright (c) 2014年 135yshr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
